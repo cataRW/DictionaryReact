5c4c7f36236d1c84ceabe237ed55f407040c8a87
cataRW <86371141+cataRW@users.noreply.github.com>
2021-11-21 07:49:01 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Dictionary from './dictionary';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <App />,
+  <Dictionary/>,
   document.getElementById('root')
 );
 
