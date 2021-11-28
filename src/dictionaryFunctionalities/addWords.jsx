e8a315be541404a81728ce429807934fbe28d54d
cataRW <86371141+cataRW@users.noreply.github.com>
2021-11-28 18:21:01 +0200

    Update addWords.jsx

--- src/dictionaryFunctionalities/addWords.jsx
+++ src/dictionaryFunctionalities/addWords.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 
 function AddWords(props) {
+  const addInList = (e) => {
+    e.preventDefault()
+    let wordToAdd = document.querySelector('.input-add').value
+    if(wordToAdd.length > 0 && !props.list.includes(wordToAdd)) {
+      props.updateWordList(wordToAdd)
+    }
+    e.currentTarget.reset()
+  }
+
   return (
-    <form onSubmit={props.updateWordList}>
+    <form onSubmit={addInList}>
       <label>
         Add word
-        <input value={props.word} onChange={props.updateWord}/>
+        <input className='input-add' placeHolder='add...'  autoFocus={false}/>
         <input type='submit' value='send'/>
       </label>
     </form>
