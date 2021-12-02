22156ee2cdf33b519afff9245618a5bd7430c564
cataRW <86371141+cataRW@users.noreply.github.com>
2021-12-02 21:55:35 +0200

    Update wordList.jsx

--- src/dictionaryFunctionalities/wordList.jsx
+++ src/dictionaryFunctionalities/wordList.jsx
@@ -1,12 +1,7 @@
 import React from 'react'
 
 function WordList(props) {
-  let list = props.list.map(item => {
-    if(item === props.word && !props.choice) {
-      return <li style={{color: 'red'}} key={item.toString()}> {item} </li>
-    }
-    return <li key={item.toString()}> {item} </li>
-  })
+  let list = props.list.map(item => <li key={item.toString()}> {item} </li>)
 
   return (
     <div>
