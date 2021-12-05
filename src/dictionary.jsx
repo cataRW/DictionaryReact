3b6ac86089e975274a298a304bc58feb7b2c27ea
cataRW <86371141+cataRW@users.noreply.github.com>
2021-12-05 14:25:48 +0200

    Update dictionary.jsx

--- src/dictionary.jsx
+++ src/dictionary.jsx
@@ -1,35 +1,24 @@
 import React, {useState} from 'react'
 import AddWords from './dictionaryFunctionalities/addWords'
 import SearchWords from './dictionaryFunctionalities/searchWords'
-import WordList from './dictionaryFunctionalities/wordList'
 
 function Dictionary() {
-  const[wordList, setWordList] = useState([])
-  const[userChoice, setUserChoice] = useState(true)
+  const[list, setList] = useState([])
 
-  const updateWordList = (word) => {
-    setWordList(prevList => [...prevList, word])
+  const updateList = (word) => {
+    if(!list.includes(word)) {
+      setList((prevList) => [...prevList, word])
+    }
   }
 
-  const updateChoice = () => {
-    let choice = userChoice ? false : true
-    setUserChoice(choice)
-  }
-
-  let display
-  if(userChoice) {
-    display = <AddWords  updateWordList={updateWordList} list={wordList} />
-  }else {
-    display = <SearchWords list={wordList} />
-  }
+  let showList = list.map((word) => <li key={word.toString()}> {word} </li>)
 
   return(
     <fieldset>
-      <legend> Dictionary </legend>
-      <button onClick={updateChoice}> {userChoice ? 'Search' : 'Add'} </button>
-      <br></br>
-      {display}
-      <WordList list={wordList} />
+      <legend>My dictionary</legend>
+      <AddWords updateList={updateList} />
+      <SearchWords list={list} />
+      <ul> {showList} </ul>
     </fieldset>
   )
 }
